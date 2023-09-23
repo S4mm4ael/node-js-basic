1d808610c2e277f6696210eb48dd4adfc888a9af
SEMION KRAPIVIN <homer1996@gmail.com>
2023-09-23 15:24:00 +0300

    feat: process running contineously

--- lessons/process.js
+++ lessons/process.js
@@ -1,1 +1,11 @@
 // console.log(process.argv)
+console.log('Process started')
+
+if (Math.random() > 0.5) {
+  while (true) {
+    console.log('Process is working')
+  }
+} else {
+  console.log('Process done')
+  process.exit()
+}
