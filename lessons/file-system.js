1be29f2ad809050dc0e2daa7011058635d661aee
SEMION KRAPIVIN <homer1996@gmail.com>
2023-09-25 10:07:08 +0300

    feat: rmdir

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -3,13 +3,22 @@ const path = require('path')
 
 console.log('start')
 
-fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
-  if (err) {
-    console.log(err)
-  }
-  else {
-    console.log('Folder is created')
-  }
-})
+// fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   else {
+//     console.log('Folder is created')
+//   }
+// })
+
+// fs.rmdir(path.resolve(__dirname, 'dir'), (err) => {
+//   if (err) {
+//     throw err
+//   }
+//   else {
+//     console.log('Deleted')
+//   }
+// })
 
 console.log('end')
