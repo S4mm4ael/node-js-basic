72a5620000908945ce21da32e76b26e718373d4f
Semion Krapivin <homer1996@gmail.com>
2023-09-27 17:24:41 +0200

    feat: add code and signal to cluster.on

--- lessons/os.js
+++ lessons/os.js
@@ -4,9 +4,18 @@ const cluster = require('cluster')
 const cpus = os.cpus()
 
 if (cluster.isMaster) {
-  for (let i = 1; i < cpus.length - 2; i++) {
+  for (let i = 1; i < cpus.length - 4; i++) {
     cluster.fork()
   }
+  cluster.on('exit', (worker, code, signal) => {
+    if (signal) {
+      console.log(`worker was killed by signal: ${signal}`);
+    } else if (code !== 0) {
+      console.log(`worker exited with error code: ${code}`);
+    } else {
+      console.log('worker success!');
+    }
+  })
 } else {
   console.log('Worker with id', process.pid)
 
