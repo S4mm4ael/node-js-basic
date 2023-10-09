9a037ff6d810b606244bf24c96cdd0389da0538e
Semion Krapivin <homer1996@gmail.com>
2023-10-09 22:58:15 +0200

    feat: add emitter.on() method

--- lessons/events.js
+++ lessons/events.js
@@ -1,18 +1,25 @@
 console.log('///Events.js has started!//')
+console.log('')
 
 const Emitter = require('events');
 
 const emitter = new Emitter()
 
-emitter.on('message', (data, second, third) => {
+emitter.once('message', (data, second, third) => {
   console.log('You send a message' + " " + data);
   console.log('Second argument' + " " + second);
 })
 
 const MESSAGE = process.argv[2] || '---'
 
-if (MESSAGE) {
-  emitter.emit('message', MESSAGE, 123)
-} else {
-  emitter.emit('message', 'Message isnt typed')
-}
+emitter.emit('message')
+emitter.emit('message')
+emitter.emit('message')
+emitter.emit('message')
+emitter.emit('message')
+
+// if (MESSAGE) {
+//   emitter.emit('message', MESSAGE, 123)
+// } else {
+//   emitter.emit('message', 'Message isnt typed')
+// }
