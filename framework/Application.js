8642463cb23e0af30b73dfe1e094dfa39cbda100
Semion Krapivin <homer1996@gmail.com>
2023-10-20 18:20:54 +0200

    feat: add separate classes for Router and Application

---
+++ framework/Application.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const EventEmitter = require('events');
+
+
+module.exports = class Application {
+  constructor() {
+    this.emitter = new EventEmitter();
+    this.server = this._createServer();
+  }
+
+  _createServer() {
+    return http.createServer((req, res) => {
+      const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
+
+      if (!emitted) {
+        res.end()
+      }
+    })
+  }
+
+}
--- lessons/server.js
+++ lessons/server.js
@@ -1,43 +1,11 @@
 const http = require('http');
 const EventEmitter = require('events');
-const PORT = 5000;
+const Router = require('../framework/Router')
 
+
+const PORT = 5000;
 const emitter = new EventEmitter();
 
-class Router {
-  constructor() {
-    this.endpoints = {}
-  }
-
-  request(method = "GET", path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {}
-    }
-    const endpoint = this.endpoints[path];
-
-    if (endpoint[method]) {
-      throw new Error(`${method} on adress ${path} already exists`)
-    }
-    endpoint[method] = handler;
-
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res)
-    })
-
-  }
-  get(path, handler) {
-    this.request("GET", path, handler)
-  }
-  post(path, handler) {
-    this.request("POST", path, handler)
-  }
-  put(path, handler) {
-    this.request("PUT", path, handler)
-  }
-  delete(path, handler) {
-    this.request("DELETE", path, handler)
-  }
-}
 
 const router = new Router();
 
@@ -49,12 +17,7 @@ router.get("/posts", (req, res) => {
 })
 
 
-const server = http.createServer((req, res) => {
-  const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-
-  if (!emitted) {
-    res.end()
-  }
+const server = http.createServer(
 
   //res.end(req.url)
 
