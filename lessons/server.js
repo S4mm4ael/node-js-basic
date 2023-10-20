e8ec258c7f85775d7dc24fd66882fba03225ac9b
Semion Krapivin <homer1996@gmail.com>
2023-10-20 18:14:27 +0200

    feat: add emitter to server

--- lessons/server.js
+++ lessons/server.js
@@ -1,24 +1,79 @@
 const http = require('http');
-
+const EventEmitter = require('events');
 const PORT = 5000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'text/html; charset=utf-8'
-  })
-  res.writeHead(200, {
-    'Content-type': 'application/json'
-  })
-  if (req.url === '/users') {
-    return res.end("<h1>USERS</h1>")
+const emitter = new EventEmitter();
+
+class Router {
+  constructor() {
+    this.endpoints = {}
   }
 
-  if (req.url === '/posts') {
-    return res.end(JSON.stringify([{
-      id: 1, name: 'Test post'
-    }]))
+  request(method = "GET", path, handler) {
+    if (!this.endpoints[path]) {
+      this.endpoints[path] = {}
+    }
+    const endpoint = this.endpoints[path];
+
+    if (endpoint[method]) {
+      throw new Error(`${method} on adress ${path} already exists`)
+    }
+    endpoint[method] = handler;
+
+    emitter.on(`[${path}]:[${method}]`, (req, res) => {
+      handler(req, res)
+    })
+
   }
-  res.end('<h1>Server is running!</h1>')
+  get(path, handler) {
+    this.request("GET", path, handler)
+  }
+  post(path, handler) {
+    this.request("POST", path, handler)
+  }
+  put(path, handler) {
+    this.request("PUT", path, handler)
+  }
+  delete(path, handler) {
+    this.request("DELETE", path, handler)
+  }
+}
+
+const router = new Router();
+
+router.get("/users", (req, res) => {
+  res.end("YOU SEND REQUEST TO /USERS")
+})
+router.get("/posts", (req, res) => {
+  res.end("YOU SEND REQUEST TO /POSTS")
+})
+
+
+const server = http.createServer((req, res) => {
+  const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
+
+  if (!emitted) {
+    res.end()
+  }
+
+  //res.end(req.url)
+
+  // res.writeHead(200, {
+  //   'Content-type': 'text/html; charset=utf-8'
+  // })
+  // res.writeHead(200, {
+  //   'Content-type': 'application/json'
+  // })
+  // if (req.url === '/users') {
+  //   return res.end("<h1>USERS</h1>")
+  // }
+
+  // if (req.url === '/posts') {
+  //   return res.end(JSON.stringify([{
+  //     id: 1, name: 'Test post'
+  //   }]))
+  // }
+  // res.end('<h1>Server is running!</h1>')
   // res.end(req.url)
 })
 
